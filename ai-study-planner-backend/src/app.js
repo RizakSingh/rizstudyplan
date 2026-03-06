5832cf5639f1c7acc7705aa7c73bf0570703c6e6
RizakSingh <srizak95@gmail.com>
2026-03-06 15:21:32 +0530

    auth route created

---
+++ ai-study-planner-backend/src/app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const cors = require("cors")
+
+const authRoutes = require("./routes/auth.routes")
+const studyPlanRoutes = require("./routes/studyPlan.routes")
+const topicRoutes = require("./routes/topic.routes")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req,res)=>{
+    res.send("AI Study Planner API Running")
+})
+
+app.use("/api/auth",authRoutes)
+app.use("/api/studyplans",studyPlanRoutes)
+app.use("/api/topics",topicRoutes)
+
+module.exports = app

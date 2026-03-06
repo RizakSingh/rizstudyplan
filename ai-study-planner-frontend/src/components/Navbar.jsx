9665c78103da4da7c7b493b9fc1d75f5657e8a44
RizakSingh <srizak95@gmail.com>
2026-03-06 16:56:31 +0530

    navbar updated

--- ai-study-planner-frontend/src/components/Navbar.jsx
+++ ai-study-planner-frontend/src/components/Navbar.jsx
@@ -1,21 +1,39 @@
-import React from 'react'
+import {Link,useNavigate} from "react-router-dom"
 
-const Navbar = () => {
-  return (
+function Navbar(){
+
+const navigate = useNavigate()
+
+const logout = ()=>{
+localStorage.removeItem("token")
+navigate("/login")
+}
+
+return(
+
 <div className="bg-blue-600 text-white p-4 flex justify-between">
 
 <h1 className="font-bold text-lg">AI Study Planner</h1>
 
-<div className="flex gap-4">
+<div className="flex gap-4 items-center">
 
 <Link to="/">Dashboard</Link>
 <Link to="/plans">Study Plans</Link>
 <Link to="/ai">AI Help</Link>
 
+<button
+onClick={logout}
+className="bg-red-500 px-3 py-1 rounded"
+>
+Logout
+</button>
+
 </div>
 
 </div>
-  )
+
+)
+
 }
 
 export default Navbar
